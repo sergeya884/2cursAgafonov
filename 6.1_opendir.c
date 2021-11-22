10d667e2e4611cbfd743d542067dc2922daa7dd6
sergeya884 <agafonov.sa@phystech.edu>
2021-11-23 00:54:40 +0300

    6.1_opendir 1.0

--- 6.1_opendir.c
+++ 6.1_opendir.c
@@ -1,3 +1,4 @@
+//Прогррамма выводит права доступа и тип всех файлов в текущем каталоге
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/types.h>
@@ -23,62 +24,60 @@ void file_access(long i) {
 //Нахождение типа файлов
 const char tipe(unsigned mod)
 {
-  switch (mod & S_IFMT) {
-    case S_IFBLK:  return 'b';
-    case S_IFCHR:  return 'c';
-    case S_IFDIR:  return 'd';
-    case S_IFIFO:  return 'p';
-    case S_IFLNK:  return 'l';
-    case S_IFREG:  return '-';
-    case S_IFSOCK: return 's';
-    default:       return '?';
-    }
+	switch (mod & S_IFMT) {
+		case S_IFBLK:  return 'b';
+		case S_IFCHR:  return 'c';
+		case S_IFDIR:  return 'd';
+		case S_IFIFO:  return 'p';
+		case S_IFLNK:  return 'l';
+		case S_IFREG:  return '-';
+		case S_IFSOCK: return 's';
+		default:       return '?';
+	}
 }
 
 //Тип директории
 char dtype(unsigned char dtype) {
 
-    switch (dtype) {
-        case DT_BLK:      return 'b'; 
-        case DT_CHR:      return 'c'; 
-        case DT_DIR:      return 'd';
-        case DT_FIFO:     return 'f';
-        case DT_LNK:      return 'l'; 
-        case DT_REG:      return 'r';
-        case DT_SOCK:     return 's';
-        case DT_UNKNOWN:  return '?';
-    }
+	switch (dtype) {
+		case DT_BLK:      return 'b'; 
+		case DT_CHR:      return 'c'; 
+		case DT_DIR:      return 'd';
+		case DT_FIFO:     return 'f';
+		case DT_LNK:      return 'l'; 
+		case DT_REG:      return 'r';
+		case DT_SOCK:     return 's';
+		case DT_UNKNOWN:  return '?';
+	}
 
-    return '?';
+	return '?';
 }
 
 
 int main() {
-    DIR *dir = opendir(".");
+	DIR *dir = opendir(".");
 
-    if (!dir) {
-        perror("Can't open current directory");
+	if (!dir) {
+		perror("Can't open current directory");
         return 1;
-    }
+	}
 
-    struct dirent* entry;
+	struct dirent* entry;
 
-    while ((entry = readdir(dir)) != NULL) {
+	while ((entry = readdir(dir)) != NULL) {
 
-        char entry_type = dtype(entry->d_type);
+		char entry_type = dtype(entry->d_type);
 
-        struct stat sb;
-        assert((lstat(entry->d_name, &sb)) == 0);
+		struct stat sb;
+		assert((lstat(entry->d_name, &sb)) == 0);
 
-        file_access(sb.st_mode);
+		file_access(sb.st_mode);
         
-        if (entry_type == '?') {
-            entry_type = tipe(sb.st_mode);
-        }
+		if (entry_type == '?') entry_type = tipe(sb.st_mode);
 
-        printf(" %c %s\n", entry_type, entry->d_name);
-    }
+		printf(" %c %s\n", entry_type, entry->d_name);
+	}
 
-    closedir(dir);
-    return 0;
+	closedir(dir);
+	return 0;
 }
