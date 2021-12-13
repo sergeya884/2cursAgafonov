5c3dcf153b173ddf175af003f5ae7c2ea5fedeb6
sergeya884 <agafonov.sa@phystech.edu>
2021-12-13 13:06:21 +0300

    6.1_opendir 1.0

--- 6.1_opendir.c
+++ 6.1_opendir.c
@@ -1,16 +1,15 @@
-//Прогррамма выводит права доступа и тип всех файлов в текущем каталоге
+//Прогррамма выводит /*права доступа и*/ тип всех файлов в текущем каталоге
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <assert.h>
-
+/*
 //Перевод прав доступа из чисел в буквы
-void file_access(long i) {
+void file_access(int i) {
 	for(int j=512; j>1; j/=8) {
         	i%=j;
-        	if (i/(j/8)==1) printf("--x");
+        	if (i/(j/8)==1)      printf("--x");
 		else if (i/(j/8)==2) printf("-w-");
 		else if (i/(j/8)==3) printf("-wx");
 		else if (i/(j/8)==4) printf("r--");
@@ -20,7 +19,7 @@ void file_access(long i) {
 	    
 	}
 }
-
+*/
 //Нахождение типа файлов
 const char tipe(unsigned mod)
 {
@@ -36,7 +35,6 @@ const char tipe(unsigned mod)
 	}
 }
 
-//Тип директории
 char dtype(unsigned char dtype) {
 
 	switch (dtype) {
@@ -59,7 +57,7 @@ int main() {
 
 	if (!dir) {
 		perror("Can't open current directory");
-        return 1;
+		return 1;
 	}
 
 	struct dirent* entry;
@@ -67,14 +65,13 @@ int main() {
 	while ((entry = readdir(dir)) != NULL) {
 
 		char entry_type = dtype(entry->d_type);
-
 		struct stat sb;
-		assert((lstat(entry->d_name, &sb)) == 0);
-
-		file_access(sb.st_mode);
-        
+		if (lstat(entry->d_name, &sb)<0) {
+			perror("F to stat");
+      			return 1;
+		}
+		//file_access(sb.st_mode);
 		if (entry_type == '?') entry_type = tipe(sb.st_mode);
-
 		printf(" %c %s\n", entry_type, entry->d_name);
 	}
 
