5cc594c0eaeaac458ede6c300b44c84bc7c7b671
sergeya884 <agafonov.sa@phystech.edu>
2021-12-18 20:08:09 +0300

    3.1_readwrite 1.0

--- 3.1_readwrite.c
+++ 3.1_readwrite.c
@@ -1,3 +1,4 @@
+//Программа копирует содержимое одного файла в другой блоками
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -10,12 +11,12 @@
 int main(int argc, char *argv[])
 {
 	if (argc != 3) {
-		fprintf(stderr, "Usage: %s read-filename write-filename\n", argv[0]);
+		fprintf(stderr, "Usage: %s <read-filename> <write-filename>\n", argv[0]);
 		return 1;
 	}
 	//rf файл для чтения, wf - для записи
 	int rf=open(argv[1], O_RDONLY);
-	char buffer[100];
+	char buffer[500];
 	if(rf <= 0) {
 		printf("Cannot open file for reading");
 		return 2;
@@ -27,14 +28,15 @@ int main(int argc, char *argv[])
 		return 3;
 	}
 
-	//циклически переписываем по 50
-	size_t i = 50;
-	while(i == 50){
-		i = read(rf, buffer, 50);
+	//циклически переписываем по 500. Если файлы большие лучше взять буфер больше, если они маленькие, но их много-взять меньше. 
+	ssize_t i;
+	while(1==1){
+		i = read(rf, buffer, sizeof(buffer));
 		if (i<0) {
 			printf("Read Error");
 			return 4;
 		}
+		if (i==0) break;
 	        if (write(wf,buffer,i)<=0){
 			printf("Write Error");	
 			return 5;
